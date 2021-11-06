3e4f2860ee7ff425161b6056d962f94530726cf5
Nicolas <nicolas.lacaze@gmail.com>
2021-11-06 22:30:46 +0100

    Display shares data in table after fetching serverless function

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  return <div>Hello world</div>;
+  const [shares, setShares] = useState([]);
+  useEffect(() => {
+    fetch("/.netlify/functions/get-shares")
+      .then((response) => response.json())
+      .then((response) => setShares(response))
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Industry</th>
+            <th>Number of years</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shares.map((share) => {
+            return (
+              <tr key={share.id}>
+                <td>{share.name}</td>
+                <td>{share.ticker}</td>
+                <td>{share.industry}</td>
+                <td>{share.numberOfYears}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
