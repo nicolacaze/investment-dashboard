65090d7b6ae5a8161784f41487a275a320b552d6
Nicolas <nicolas.lacaze@gmail.com>
2021-11-09 16:44:49 +0100

    Make AJAX request for file upload with form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,39 @@
 import React, { useState, useEffect } from "react";
 
+function encode(data) {
+  const formData = new FormData();
+
+  for (const key of Object.keys(data)) {
+    formData.append(key, data[key]);
+  }
+
+  return formData;
+}
+
 const App = () => {
   const [shares, setShares] = useState([]);
+  const [form, setForm] = useState({});
+
+  const handleChange = (e) => {
+    setForm({ [e.target.name]: e.target.value });
+  };
+
+  const handleAttachment = (e) => {
+    setForm({ [e.target.name]: e.target.files[0] });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const fields = e.target;
+    fetch("/", {
+      method: "POST",
+      body: encode({
+        "form-name": fields.getAttribute("name"),
+        ...form,
+      }),
+    }).catch((error) => console.error(error));
+  };
+
   useEffect(() => {
     fetch("/.netlify/functions/get-shares")
       .then((response) => response.json())
@@ -32,26 +64,28 @@ const App = () => {
           })}
         </tbody>
       </table>
-      <form name="contact" method="post">
+      <form name="contact" method="post" onSubmit={handleSubmit}>
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
-            Your Name: <input type="text" name="name" />
+            Your Name: <input type="text" name="name" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+            Your Email:{" "}
+            <input type="email" name="email" onChange={handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message"></textarea>
+            Message:{" "}
+            <textarea name="message" onChange={handleChange}></textarea>
           </label>
         </p>
         <p>
           <label>
-            File: <input type="file" name="excel" />
+            File: <input type="file" name="excel" onChange={handleAttachment} />
           </label>
         </p>
         <p>
