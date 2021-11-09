59cafec53b7d940e885eb6242de05412823bd925
Nicolas <nicolas.lacaze@gmail.com>
2021-11-09 16:09:50 +0100

    Add logs for debugging

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
 exports.handler = async (event, context) => {
+  console.log("FROM SUBMISSION FUNCTION");
+  console.log(event);
+  console.log(context);
   return {
     statusCode: 200,
     body: JSON.stringify(event),
