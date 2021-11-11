d0ea1d4ffd7287b0bc0113aa1f1c34b78347ea70
Nicolas <nicolas.lacaze@gmail.com>
2021-11-11 19:42:26 +0100

    Remove all shares from DB before adding new bulk

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,32 +2,53 @@
 const { queryHasura } = require("./utils/hasura");
 const { parseExcel } = require("./utils/parser");
 
-exports.handler = async (event, context) => {
+exports.handler = async (event, context, callback) => {
   // const body = JSON.parse(event.body);
   // const excelFileURL = body.payload.human_fields.Excel;
   const data = await parseExcel();
 
-  // const result = await queryHasura({
-  //   query: `
-  //     mutation addShares($objects: [shares_insert_input!]! ) {
-  //       insert_shares(objects: $objects) {
-  //         returning {
-  //           id
-  //           industry
-  //           name
-  //           numberOfYears
-  //           ticker
-  //         }
-  //       }
-  //     }
-  //   `,
-  //   variables: {
-  //     objects: data,
-  //   },
-  // });
+  // Remove all shares from table before adding new ones
+  await queryHasura({
+    query: `
+      mutation removeShares {
+        delete_shares(where: {}) {
+          affected_rows
+        }
+      }
+    `,
+  });
 
-  return {
+  const result = await queryHasura({
+    query: `
+      mutation addShares($objects: [shares_insert_input!]! ) {
+        insert_shares(objects: $objects) {
+          affected_rows
+          returning {
+            id
+            name
+            ticker
+            industry
+            numberOfYears
+            price
+            dividendYield
+            annualizedDividend
+            threeYearsDividendGrowthRate
+            fiveYearsDividendGrowthRate
+            tenYearsDividendGrowthRate
+            fairValue
+            freeCashFlowPerShare
+            priceEarningsRatio
+          }
+        }
+      }
+    `,
+    variables: {
+      objects: data,
+    },
+  });
+
+  return callback(null, {
     statusCode: 200,
-    body: "wip", // JSON.stringify(result),
-  };
+    body: JSON.stringify(result),
+  });
 };
