50c77bc2bd0acb77ed3399af19972432ea6b4bb9
Nicolas <nicolas.lacaze@gmail.com>
2021-11-11 18:17:12 +0100

    Apply investment method filters before returning data

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,32 +2,32 @@
 const { queryHasura } = require("./utils/hasura");
 const { parseExcel } = require("./utils/parser");
 
-exports.handler = async (event, context, callback) => {
-  const body = JSON.parse(event.body);
-  const excelFileURL = body.payload.human_fields.Excel;
-  const data = await parseExcel(excelFileURL);
+exports.handler = async (event, context) => {
+  // const body = JSON.parse(event.body);
+  // const excelFileURL = body.payload.human_fields.Excel;
+  const data = await parseExcel();
 
-  const result = await queryHasura({
-    query: `
-      mutation addShares($objects: [shares_insert_input!]! ) {
-        insert_shares(objects: $objects) {
-          returning {
-            id
-            industry
-            name
-            numberOfYears
-            ticker
-          }
-        }
-      }
-    `,
-    variables: {
-      objects: data,
-    },
-  });
+  // const result = await queryHasura({
+  //   query: `
+  //     mutation addShares($objects: [shares_insert_input!]! ) {
+  //       insert_shares(objects: $objects) {
+  //         returning {
+  //           id
+  //           industry
+  //           name
+  //           numberOfYears
+  //           ticker
+  //         }
+  //       }
+  //     }
+  //   `,
+  //   variables: {
+  //     objects: data,
+  //   },
+  // });
 
-  return callback(null, {
+  return {
     statusCode: 200,
-    body: JSON.stringify(result),
-  });
+    body: "wip", // JSON.stringify(result),
+  };
 };
