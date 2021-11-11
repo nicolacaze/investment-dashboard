a6125f253cdb566f29acf6604a2c5a686c2d5dc9
Nicolas <nicolas.lacaze@gmail.com>
2021-11-11 19:41:54 +0100

    Add more fields to schema

--- functions/utils/schema.js
+++ functions/utils/schema.js
@@ -16,6 +16,42 @@ const schema = {
     prop: "numberOfYears",
     type: Number,
   },
+  Price: {
+    prop: "price",
+    type: Number,
+  },
+  "Div Yield": {
+    prop: "dividendYield",
+    type: Number,
+  },
+  Annualized: {
+    prop: "annualizedDividend",
+    type: Number,
+  },
+  "DGR 3Y": {
+    prop: "threeYearsDividendGrowthRate",
+    type: Number,
+  },
+  "DGR 5Y": {
+    prop: "fiveYearsDividendGrowthRate",
+    type: Number,
+  },
+  "DGR 10Y": {
+    prop: "tenYearsDividendGrowthRate",
+    type: Number,
+  },
+  "Fair Value": {
+    prop: "fairValue",
+    type: String,
+  },
+  "CF/Share": {
+    prop: "freeCashFlowPerShare",
+    type: Number,
+  },
+  "P/E": {
+    prop: "priceEarningsRatio",
+    type: Number,
+  },
 };
 
 exports.schema = schema;
